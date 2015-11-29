3faf15705705c4868fcf6db981aef4d5edc80bbf
roryk <rory.kirchner@gmail.com>
2015-11-28 21:18:45 -0500

    Update with sorting/filtering.

--- radar-convert-annotations.py
+++ radar-convert-annotations.py
@@ -7,16 +7,16 @@ if __name__ == "__main__":
     parser.add_argument("converted")
     args = parser.parse_args()
 
-    original = pd.read_csv(args.original, delimiter="\t")
+    original = pd.read_csv(args.original, delimiter="\t", dtype=str)
     original.columns = ['chromosome', 'start', 'end', 'gene', 'strand',
                         'annot1', 'annot2', 'alu?', 'non_alu_repetitive?',
                         'conservation_chimp', 'conservation_rhesus',
                         'conservation_mouse']
-    converted = pd.read_csv(args.converted, delimiter="\t", header=None)
+    converted = pd.read_csv(args.converted, delimiter="\t", header=None, dtype=str)
     converted.columns = ['orig_chr', 'orig_start', 'orig_end', 'skip',
                          'new_chr', 'new_start', 'new_end']
     merged = original.merge(converted, left_on=['chromosome', 'start'],
-                            right_on=['orig_chr', 'orig_start'])
+                            right_on=['orig_chr', 'orig_start'], how="right")
     merged = merged[['new_chr', 'new_start', 'new_end', 'gene', 'strand',
                      'annot1', 'annot2', 'alu?', 'non_alu_repetitive?',
                      'conservation_chimp', 'conservation_rhesus',
@@ -25,5 +25,9 @@ if __name__ == "__main__":
                         'annot1', 'annot2', 'alu?', 'non_alu_repetitive?',
                         'conservation_chimp', 'conservation_rhesus',
                         'conservation_mouse']
-    import ipdb
-    ipdb.set_trace()
+    merged = merged.dropna()
+    merged['chromosome'] = merged['chromosome'].str[3:]
+    merged['start'] = merged['start'].astype(int)
+    merged = merged.sort(["chromosome", "start"])
+    merged['chromosome'] = 'chr' + merged['chromosome']
+    merged.to_csv("out.bed", sep="\t", index=False)
